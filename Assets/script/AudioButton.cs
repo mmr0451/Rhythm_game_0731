bc83b53ff7c0135685afb48285c2092813e44332
hakushika <sanuki451@gmail.com>
2021-08-13 16:11:22 +0900

    0813_音とアニメの同期調整

--- Assets/script/AudioButton.cs
+++ Assets/script/AudioButton.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
+
 public class AudioButton : MonoBehaviour
 {
     // 音とアイコン
@@ -10,11 +11,14 @@ public class AudioButton : MonoBehaviour
 
     public Animator AnimCon;
 
+    private float PlayTime;
+
     
     // 持っている音と、アイコンを持つ
     void Start()
     {
         audioSource = this.gameObject.GetComponent<AudioSource>();
+        AnimCon.enabled = false;
     }
 
 
@@ -25,12 +29,44 @@ public class AudioButton : MonoBehaviour
         {
             //audioSource.time = 0.0f;
             audioSource.volume = 1.0f;
-            AnimCon.SetBool("icon_bool",true);
+            //AnimCon.SetBool("icon_bool",true);
+            AnimCon.enabled = true;
+
+            PlayTime = Mathf.Lerp(0, 1, Mathf.Repeat(audioSource.time, 2)); 
+            AnimCon.Play(AnimCon.GetCurrentAnimatorStateInfo(0).shortNameHash, 0, PlayTime);
+
+            /*
+            PlayTime = Mathf.Lerp(0, 1, Mathf.Repeat(audioSource.time, 2)); 
+            Debug.Log("補完値　　" + PlayTime);
+            
+            AnimCon.Play(AnimCon.GetCurrentAnimatorStateInfo(0).shortNameHash, 0, PlayTime);
+            */
+
+            //PlayTime = Mathf.Lerp(1, 0, Mathf.Repeat(audioSource.time, 2)); 
+            //Debug.Log("補完値　　" + PlayTime);
+
+            /*
+            AnimatorStateInfo animeStateInfo = AnimCon.GetCurrentAnimatorStateInfo(0);
+            float degrees = Mathf.Lerp(2, 0, Mathf.Repeat(animeStateInfo.normalizedTime, 1)); 
+            //AnimCon.Play(AnimCon.GetCurrentAnimatorStateInfo(0).shortNameHash, 0, Random.Range(0f, 1f));
+            AnimCon.Play(AnimCon.GetCurrentAnimatorStateInfo(0).shortNameHash, 0, degrees);
+    
+            Debug.Log("アニメの時間   " + degrees);
+            */
         }
         else
         {
             audioSource.volume = 0.0f;
-            AnimCon.SetBool("icon_bool",false);
+            //AnimCon.SetBool("icon_bool",false);
+            AnimCon.enabled = false;
         }
     }
+
+    
+    void Update() {
+        //PlayTime = Mathf.Lerp(1, 0, Mathf.Repeat(audioSource.time, 2)); 
+        //PlayTime = Mathf.Lerp(0, 1, Mathf.Repeat(audioSource.time, 2)); 
+        //Debug.Log("補完値　　" + PlayTime);
+    }
+    
 }
